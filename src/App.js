c10c1456151213ce244ef006a863af4a381f5e20
Michael Salerno <msalerno1993@gmail.com>
2023-03-01 17:16:52 -0500

    finished directory page/components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import './categories.styles.scss'
+import Directory from "./components/directory/directory.component";
+
 
 function App() {
-  const categories = [
-    { id: 1, title: "Hats" },
-    { id: 2, title: "Jackets" },
-    { id: 3, title: "Sneakers" },
-    { id: 4, title: "Womens" },
-    { id: 5, title: "Mens" },
-  ];
-  return (
-    <div className="catergories-container">
-      {categories.map(({ title, id}) =>(
-        <div key={id} className="category-container">
-          <div className='background-image'/>
-          <div className="category-body-container">
-            <h2 className=""> {title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
+  return(
+    <div>
+      <Directory />
     </div>
-  );
+  )
 }
 
 export default App;
