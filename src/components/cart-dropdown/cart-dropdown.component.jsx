924271538c489fc2e80089c4d92807be1bbcf149
Michael Salerno <msalerno1993@gmail.com>
2023-03-08 17:50:17 -0500

    adding addtocart feature/styled

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,31 @@
-import Button from '../button/button.component'
-import './cart-dropdown.styles.scss'
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
+
   return (
     <div className='cart-dropdown-container'>
-        <div className="cart-items"></div>
-        <Button buttonType='inverted'>Checkout</Button>
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
-  )
-}
+  );
+};
 
-export default CartDropdown
+export default CartDropdown;
